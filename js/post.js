50f1a06af1bcdffa9d0e2fdf65e920197d49f0f5
Jan Kulik <neavvy550@gmail.com>
2019-03-20 19:19:38 +0100

    Add files via upload

--- js/post.js
+++ js/post.js
@@ -6,7 +6,7 @@ converter.setOption('ghMentionsLink', 'https://steemit.com/@{u}');
 
 var querystring = location.search;
 var parentAuthor = querystring.split('/')[0];
-parentAuthor = parentAuthor.slice(1, author.length);
+parentAuthor = parentAuthor.slice(1, parentAuthor.length);
 var parentPermlink = querystring.split('/')[1];
 
 var token;
